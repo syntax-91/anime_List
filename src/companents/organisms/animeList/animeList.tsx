1763e8ef86fd3a8bce32e2457a7f0a26d31d7582
syntax <dlscam112@gmail.com>
2025-04-21 02:58:14 +0500

    f213er

--- src/companents/organisms/animeList/animeList.tsx
+++ src/companents/organisms/animeList/animeList.tsx
@@ -74,13 +74,13 @@ import shareIcon from './../../../assets/share.png'
 						</div>
 						{/**/ }
 						<div 
-						 onClick={() => setCopy(`https://anime-list-sandy-nine.vercel.app/anime/${data.mal_id}`) }
+						 onClick={() => setCopy(`/anime/${data.mal_id}`) }
 						 className='absolute top-5 right-20
 						w-[40px] h-[40px] p-[3px] rounded-[5px]
 						cursor-pointer  blur-white'>
 								<img src={shareIcon} alt="ERROR" />
 						</div>
-
+ 
 					<div className='
 					
 					 rounded-2xl'>
