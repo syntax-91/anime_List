74d83bc397acc8fca73ae412f480d46bc5900512
syntax <dlscam112@gmail.com>
2025-06-30 23:38:44 +0500

    inf: огоо норм норм но можно улучшать)

--- frontend/src/components/organisms/header/header.tsx
+++ frontend/src/components/organisms/header/header.tsx
@@ -1,5 +1,4 @@
-import { gsap } from 'gsap/gsap-core'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Logo } from '../../molecules/logo/logo'
 import { Menu } from '../../molecules/menu/menu'
 import { SearchHeader } from '../../molecules/search/search'
@@ -7,17 +6,13 @@ import { Nav } from '../nav/nav'
 
 export function Header() {
 	const [isOpenMenu, setIsOpenMenu] = useState(false)
-	const boxRef = useRef(null)
 
-	useEffect(() => {
-		gsap.fromTo(boxRef.current, { x: -40, opacity: 0 }, { opacity: 1, x: 0 })
-	}, [])
+	useEffect(() => {}, [])
 
 	return (
 		<header
-			ref={boxRef}
 			className='w-[90%] 
-		flex py-[25px]  scale-40
+		flex py-[25px]  ltr
 		mx-auto justify-between items-center'
 		>
 			<Logo />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './shared/index.css'
 import './shared/styles/animations.css'
 import './shared/styles/globalStyles.css'
+import './shared/styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
